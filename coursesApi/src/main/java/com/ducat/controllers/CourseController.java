c2ff68f4301b6306799de2b5a3256115f86ddb4a
Neeraj Kumar <nraj_adi@yahoo.co.in>
2019-09-15 14:12:17 +0530

    1tom relation table mapping with api

---
+++ coursesApi/src/main/java/com/ducat/controllers/CourseController.java
@@ -0,0 +1,46 @@
+package com.ducat.controllers;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ducat.entities.Course;
+import com.ducat.repos.CourseDao;
+
+@RestController
+public class CourseController {
+
+	@Autowired
+	private CourseDao dao;
+	// To fetch all courses.
+	@GetMapping(value="/courses", produces="application/json")
+	public Iterable<Course> getAllCourses() {
+		return dao.findAll();
+	}
+	
+	// To fetch a course using its id.
+	@GetMapping(value="/courses/{id}", produces="application/json")
+	public Course byId( 
+		@PathVariable	int id) {
+		Course course = null;
+		Optional<Course> opt = dao.findById(id);	
+		if (opt.isPresent())
+			course = opt.get();
+		
+		return course;
+	}
+	
+	// To save a course.
+	@PostMapping(value="/courses", produces="text/plain",
+			consumes="application/json")
+	public String save(
+		@RequestBody Course course) {
+		dao.save(course);
+		return "successfully saved.";
+	}
+}
